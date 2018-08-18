ab5e3c0322f199d392a5017d5b823a5e9eb0fe73
Allan Dantas <allancruz716@gmail.com>
2018-08-17 22:54:56 -0300

    JSX and Components

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class HelloWorld extends Component {
 	render() {
+		const result = 4
+		const style = {
+			color: '#ff0000',
+			fontSize: 50,
+			backgroundColor: '#000000'
+		}
 		return (
 			<div className="my-class-from-react">
-				<h1>Hello World React</h1>
+				{/* This is my first componet */}
+				<h1 style={style}>Hello World React</h1>
+				<h4>{ result === 2 * 2 ? <div><ul><li>TRUE</li></ul></div> : <div><ul><li>FALSE</li></ul></div>}</h4>
 			</div>
 		)
 	}
