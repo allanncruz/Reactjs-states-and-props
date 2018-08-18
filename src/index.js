83b4c3a7dec86dcf8559b8bc02bb65caa8e91925
Allan Dantas <allancruz716@gmail.com>
2018-08-17 22:39:50 -0300

    First component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import HelloWorld from './HelloWorld'
+
+ReactDOM.render(
+		<HelloWorld />,
+		document.getElementById("root")
+	)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
