22bbd1926d670782836d5186b32c7f12d59b594c
Allan Dantas <allancruz716@gmail.com>
2018-08-18 21:06:12 -0300

    Props Validation

---
+++ src/PropsValidation.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class PropsValidation extends Component{
+	constructor(props) {
+		super(props)
+
+		console.log(typeof this.props.name)
+	}
+	render(){
+		return(
+			<div>
+				<h1>{this.props.name}</h1>
+				<h1>{this.props.age}</h1>
+				<h1>{this.props.height}</h1>
+			</div>
+		)
+	}
+}
+
+PropsValidation.propTypes = {
+	name  : PropTypes.string,
+	age   : PropTypes.number,
+	height: PropTypes.number,
+	func  : PropTypes.func,
+	obj   : PropTypes.object,
+	arr   : PropTypes.array
+}
+
+PropsValidation.defaultPros = {
+	name   : 'Allan Cruz',
+	age    : 21,
+	height : 1.75
+}
+
+export default  PropsValidation
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 import HelloWorld from './HelloWorld'
+import PropsValidation from './PropsValidation'
 
 ReactDOM.render(
-		<HelloWorld txt = 'Hello World From Component React.js' />,
+		// <HelloWorld txt = 'Hello World From Component React.js' />,
+		<PropsValidation name="123" age="29" height="1.76" />,
 		document.getElementById("root")
 	)
 
